638e5900351a974f39c2b63a509bbbcb8f7cb34b
Jackson Lorran <computadordb@gmail.com>
2024-04-24 09:45:57 -0300

    add: adding one workout and one section to dowload archives to study/1

--- src/components/Treinos/index.js
+++ src/components/Treinos/index.js
@@ -27,7 +27,15 @@ function Treinos() {
             "endpoint": 'estudo_artigo1',
             "descricao": "Artigo voltado a informação sobre treinamento de força para basquete",
             "tipo": "estudo",
-            "downloadLink":"https://d1wqtxts1xzle7.cloudfront.net/65604937/PDF_LIVRO_0K_17-libre.pdf?1612454600=&response-content-disposition=inline%3B+filename%3DBASQUETE_TREINAMENTO_DA_FORCA_FUNCIONAL.pdf&Expires=1713963742&Signature=BftBDDAklpn2Hr1wBMY1jU~pMEZjOmoDwE9IE-NaZwkpJaW8oO-yLXnCfOeVpWYQUMdyoXyr48vLw5vo65~u4R5tt~gUIJnuWNPauos7yXQfrJDvL34y8lFrKtAvtavpo0OgM~Z7kf0XcMkLVBlTHWdLVQgeSsPnspwwjgn6tUHFUXnxbjJCxR5Xa~wp65c0lXexVJAaTWrHAFa1bkgmt144n7fZGsAgO1FcNKXcN6XaC6w-X7rDtK3u-M8tVqouzuorHDjZpv6l3yvGbsqbvy6GVvDzcaBMFQsModpzJYBGkFcZE9zEj4G7qINyBuFA-6ykBv2xNJyhLr4LjH~bdg__&Key-Pair-Id=APKAJLOHF5GGSLRBV4ZA",
+            "downloadLink":"/archives/BASQUETE_TREINAMENTO_DA_FORCA_FUNCIONAL.pdf",
+        },
+        {
+            "img": "https://www.infolivros.org/wp-content/uploads/2021/08/livros-basquetebol.webp?ezimgfmt=ngcb2/notWebP",
+            "titulo": "Treino de força: Perspectiva de preparadores físicos em equipes de alto rendimento",
+            "endpoint": 'estudo_artigo1',
+            "descricao": "Visão de preparadores físicos do alto rendimento, quanto ao treinamento de força. ",
+            "tipo": "estudo",
+            "downloadLink":"/archives/Treino_da_Força_no_Basquetebol_A_Perspetiva_de_Preparadores_Físicos_de_Equipas_de_Alto_Rendimento.pdf",
         },
         {
             "img": "/imgs/kirie.jpg",
